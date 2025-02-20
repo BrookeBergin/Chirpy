70cc8ab093ce24e072f34ae1ba0cf9be8a4b1adb
kbl48 <kbl48@georgetown.edu>
2025-02-20 03:18:35 +0000

    UserService implemented

--- src/main/java/edu/georgetown/bll/user/UserService.java
+++ src/main/java/edu/georgetown/bll/user/UserService.java
@@ -9,17 +9,22 @@ import edu.georgetown.dao.*;
 public class UserService {
 
     private static Logger logger;
+    private Vector<Chirper> users;
 
     public UserService(Logger log) {
         logger = log;
         logger.info("UserService started");
+        users = new Vector<>();
     }
 
     public Vector<Chirper> getUsers() {
         // not implemented; you'll need to change this
-        return null;
+        return users;
     }
 
+    public void registerUser(Logger newUser){
+        
+    }
     // methods you'll probably want to add:
     //   registerUser
     //   loginUser
