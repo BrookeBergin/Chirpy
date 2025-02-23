8e6ba2dcc7044e098c66b3f2a696e9cfd2cdf8ae
kbl48 <kbl48@georgetown.edu>
2025-02-23 17:16:04 +0000

    regist and login functions

--- src/main/java/edu/georgetown/bll/user/UserService.java
+++ src/main/java/edu/georgetown/bll/user/UserService.java
@@ -22,8 +22,46 @@ public class UserService {
         return users;
     }
 
-    public void registerUser(Logger newUser){
+    public String registerUser(String username, String password, String confirmPass){
+        if(username == null || username.isEmpty()){
+            logger.warning("Username cannot be empty.");
+            return("Username cannot be empty");
+        }
         
+        for(Chirper user : users){
+            if(user.getUsername() == username){
+                logger.warning("Username has already been taken.");
+                return("Username cannot be empty");
+            }
+        }
+
+        if(password == null || password.isEmpty()){
+            logger.warning("Password cannot be empty.");
+            return("Password cannot be empty");
+        }
+
+        if(password != confirmPass){
+            logger.warning("Passwords do not match.");
+            return("Passwords do not match.");
+        }
+        Chirper newUser = new Chirper(username, password);
+        users.add(newUser);
+
+        logger.info("User registered successfully.");
+        return("User registered successfully: " + newUser.getUsername());
+    }
+
+    public String loginUser(String username, String password){
+        for(Chirper user : users){
+            if(username == user.getUsername()){
+                if(user.checkPassword(password)){
+                    logger.info("User logged in successfully.");
+                    return("User logged in successfully: " + username);
+                }
+            }
+        }
+        logger.warning("Could not find user.");
+        return("Could not find user.");
     }
     // methods you'll probably want to add:
     //   registerUser
