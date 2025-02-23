890b8e2998bada097c336c107b1f995b47b617b0
Anura633 <as4965@georgetown.edu>
2025-02-23 17:22:39 +0000

    newstuff

--- src/main/java/edu/georgetown/bll/user/UserService.java
+++ src/main/java/edu/georgetown/bll/user/UserService.java
@@ -8,17 +8,48 @@ import edu.georgetown.dao.*;
 
 public class UserService {
 
-    private static Logger logger;
+ private static Logger logger;
+ private static Vector<Chirper> registeredUsers = new Vector<>();
 
     public UserService(Logger log) {
         logger = log;
         logger.info("UserService started");
+    } 
+   //Register a user
+   public Vector<Chirper> getUsers() {   
+    return registeredUsers;
+}
+public boolean registerUser(String username, String password){
+    //checking if username is already taken
+    for (Chirper user: registeredUsers){
+        if (user.getUsername().equals(username)){
+            logger.warning("Registration failed: Username is already taken");
+            return false;
+        }
     }
+    Chirper newUser = new Chirper(username, password);
+    registeredUsers.add(newUser);
+    logger.info("user registered sucessfully");
+    return true;
+}
 
-    public Vector<Chirper> getUsers() {
-        // not implemented; you'll need to change this
-        return null;
+
+ //Log in a user
+ public boolean loginUser(String username, String password){
+    for (Chirper user: registeredUsers){
+        if (user.getUsername().equals(username)){
+            if (user.checkPassword(password)){
+                logger.info("Login successful for user: " + username);
+                return true;//the login was successful
+            } else{
+                logger.warning("Login failed due to incorrect password for " + username);
+                return false; //incorrect password entered
+            }
+        }
     }
+    logger.warning("Login failed: username not found");
+    return false; //username was not found
+ }
 
     // methods you'll probably want to add:
     //   registerUser
