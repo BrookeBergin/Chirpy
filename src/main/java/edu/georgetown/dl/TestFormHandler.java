23b8ef5a570a6038d2d76115e61bd7f72c3bc423
dianecho1228 <dmc320@georgetown.edu>
2025-02-28 18:33:11 +0000

    fixed some stuff

--- src/main/java/edu/georgetown/dl/TestFormHandler.java
+++ src/main/java/edu/georgetown/dl/TestFormHandler.java
@@ -1,5 +1,6 @@
 package edu.georgetown.dl;
 
+import java.util.Vector;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.StringWriter;
@@ -16,6 +17,7 @@ public class TestFormHandler implements HttpHandler {
     final String FORM_PAGE = "formtest.thtml";
     private Logger logger;
     private DisplayLogic displayLogic;
+    private UserService userService;
 
     public TestFormHandler(Logger log, DisplayLogic dl, UserService userService) {
         logger = log;
@@ -30,37 +32,15 @@ public class TestFormHandler implements HttpHandler {
         // dataModel will hold the data to be used in the template
         Map<String, Object> dataModel = new HashMap<String, Object>();
 
-        Map<String, String> dataFromWebForm = displayLogic.parseResponse(exchange);
 
-        // // if the web form contained a username, add it to the data model
-        // if (dataFromWebForm.containsKey("username")) {
-        //     dataModel.put("username", dataFromWebForm.get("username"));
-        // }
-        // Extract values from the form
-        String username = dataFromWebForm.get("username");
-        String password = dataFromWebForm.get("password");
+        Vector<String> usernameVector = new Vector<>();
 
-        // If the form was submitted, attempt to log in
-        if ("POST".equalsIgnoreCase(exchange.getRequestMethod())) {
-            logger.info("Login POST detected");
-            if (username != null && password != null) {
-                boolean loginSuccess = userService.loginUser(username, password);
-
-                if (loginSuccess) {
-                    dataModel.put("message", "Login successful!");
-                    addUserCookie(exchange, username);  // Optionally set a user cookie
-                } else {
-                    dataModel.put("message", "Login failed. Please check your credentials.");
-                }
-            }
-        } else {
-            dataModel.put("message", " ");
+        for (int i = 0; i < userService.getUsers().size(); i++){
+                usernameVector.add(userService.getUsers().get(i).getUsername());
         }
 
-        // Ensure message is always set
-        if (!dataModel.containsKey("message")) {
-            dataModel.put("message", "");
-        }
+        dataModel.put("usernames", usernameVector);
+
         
         // sw will hold the output of parsing the template
         StringWriter sw = new StringWriter();
@@ -70,11 +50,6 @@ public class TestFormHandler implements HttpHandler {
 
         // set the type of content (in this case, we're sending back HTML)
         exchange.getResponseHeaders().set("Content-Type", "text/html");
-        
-        // if we have a username, set a cookie with the username.
-        if (dataFromWebForm.containsKey("username")) {
-            displayLogic.addCookie(exchange, "username", dataFromWebForm.get("username"));
-        }
 
         // send the HTTP headers
         exchange.sendResponseHeaders(200, (sw.getBuffer().length()));
