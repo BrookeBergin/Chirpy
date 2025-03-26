7430cd60e5f8be3dabf216d2ab5f33a762ce9002
BrookeBergin <brookebergin27@gmail.com>
2025-03-26 03:08:30 +0000

    feedPage + handler created

--- src/main/java/edu/georgetown/Chirpy.java
+++ src/main/java/edu/georgetown/Chirpy.java
@@ -26,6 +26,7 @@ import edu.georgetown.dl.DefaultPageHandler;
 import edu.georgetown.dl.DisplayLogic;
 import edu.georgetown.dl.ListCookiesHandler;
 import edu.georgetown.dl.RegisterPageHandler;
+import edu.georgetown.dl.FeedHandler;
 import edu.georgetown.dl.TestFormHandler;
 
 public class Chirpy {
@@ -83,9 +84,10 @@ public class Chirpy {
 
             // each of these "contexts" below indicates a URL path that will be handled by
             // the service. The top-level path is "/", and that should be listed last.
-            server.createContext("/formtest/", new TestFormHandler(logger, displayLogic, userService));
-            server.createContext("/registerPage/", new RegisterPageHandler(logger, displayLogic, userService));
+            server.createContext("/formtest/", new TestFormHandler(logger, displayLogic));
+            server.createContext("/registerPage/", new RegisterPageHandler(logger, displayLogic));
             server.createContext("/listcookies/", new ListCookiesHandler(logger, displayLogic));
+            server.createContext("/feedPage/", new FeedHandler(logger, displayLogic));
             server.createContext("/", new DefaultPageHandler(logger, displayLogic));
             // you will need to add to the above list to add new functionality to the web
             // service.  Just make sure that the handler for "/" is listed last.
